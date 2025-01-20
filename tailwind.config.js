10ee4f124c20bb058c1cd545a4a31cee70d9da1a
Nilesh Rathod <nrrathod223@gmail.com>
2025-01-20 15:26:24 +0530

    Add feedback changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,44 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+
+      'xsxs': '361px',
+      // => @media (min-width: 360px) { ... }
+
+      "small-device": "400px",
+      // => @media (min-width:400px) {...}
+
+      'xss': '480px',
+      // => @media (min-width: 480px) { ... }
+
+      'xs': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'mmd': '865px',
+      // => @media (min-width: 865px) { ... }
+
+      'slg': '992px',
+      // => @media (min-width: 880px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '1x1': '1350px',
+      // => @media (min-width: 1350px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
